d4970a256a919f02c98693241e8e50538ef544a0
Luiz Felipe Pedrosa Cavalcante <54713427+gitBiohazard@users.noreply.github.com>
2022-10-16 15:29:02 -0300

    update: clean react start

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+// React
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// CSS
 import './index.css';
+
+//  Components
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
