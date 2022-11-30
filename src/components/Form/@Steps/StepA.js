271422e0e17d0c8af621c9ba674185adedae69d4
Luiz Felipe Pedrosa Cavalcante <54713427+gitBiohazard@users.noreply.github.com>
2022-11-30 18:23:12 -0300

    update: gas fee

--- src/components/Form/@Steps/StepA.js
+++ src/components/Form/@Steps/StepA.js
@@ -1,115 +1,196 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-
-import React from 'react'
+import React from "react";
 // CSS
-import "./Steps.css"
+import "./Steps.css";
 // Components
-import InputBRL from '../InputBRL/InputBRL'
-import InputCoin from '../InputCoin/InputCoin'
-import Summary from '../Summary/Summary'
+import InputBRL from "../InputBRL/InputBRL";
+import InputCoin from "../InputCoin/InputCoin";
+import Summary from "../Summary/Summary";
 // Contexts
-import TokenContext from '../../../contexts/TokenContext'
-import SelectorContext from '../../../contexts/SelectorContext'
+import TokenContext from "../../../contexts/TokenContext";
+import SelectorContext from "../../../contexts/SelectorContext";
+import { ethers } from "ethers";
 
 const StepA = () => {
-    // Context
-    const { token } = React.useContext(TokenContext)
-    // State
-    const [isOpen, setIsOpen] = React.useState(true)
+  // Context
+  const { token } = React.useContext(TokenContext);
+  // State
+  const [isOpen, setIsOpen] = React.useState(true);
 
-    // Animation
-    const entranceConfig = {
-        animation: "entrance .5s ease-out"
-    }
-    // States
-    const [tax, setTax] = React.useState(0)
-    const [price, setPrice] = React.useState(0)
-    const [gasFee, setGasFee] = React.useState(0)
-    const [counter, setCounter] = React.useState(30)
-    const [valuation, setValuation] = React.useState(0)
-    const [payoff, setPayoff] = React.useState(() => {
-        const getItem = localStorage.getItem("buyValue")
-        return getItem ? getItem : 0
-    })
+  // Animation
+  const entranceConfig = {
+    animation: "entrance .5s ease-out",
+  };
+  // States
+  const [tax, setTax] = React.useState(0);
+  const [price, setPrice] = React.useState(0);
+  const [gasFee, setGasFee] = React.useState(0);
+  const [counter, setCounter] = React.useState(30);
+  const [asyncFee, setAsyncFee] = React.useState(0);
+  const [valuation, setValuation] = React.useState(0);
+  const [payoff, setPayoff] = React.useState(() => {
+    const getItem = localStorage.getItem("buyValue");
+    return getItem ? getItem : 0;
+  });
 
-    // Constant
-    const mainToken = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174"
+  // Constant
+  const mainToken = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";
 
-    // Functions
-    const handleConversion = async (e) => {
-        const number = +(e.target.value.replaceAll(".", "").replace(",", "."))
-        setPayoff(number)
-        getFee(number)
-    }
+  // Functions
+  const handleConversion = async (e) => {
+    const number = +e.target.value.replaceAll(".", "").replace(",", ".");
+    setPayoff(number);
+    getFee(number);
+  };
 
-    const getFee = (number) => { // Função recebe o valor digitado
-        // Inflações
-        let value = number // Definindo a variável value sendo igual ao valor digitado
-        let taxValue = 0 // Iniciando cálculo da taxa
-        taxValue += number * .017 // Adicionando ao total da taxa o valor da taxa de processamento
-        if (number > 62.5) taxValue += number * .008 // Adicionando ao total da taxa o valor da taxa complexa
-        else taxValue += 0.5 // Adicionando ao total da taxa o valor da taxa fixa
-        if (value >= 20) { setTax(taxValue); setGasFee(.20) } // Setando a taxa de processamento variável e gás da rede
-        else { setTax(0); setGasFee(0) } // Zerando taxas e gás da rede em caso do valor inserido ser inválido
-
-        // Setando o valor
-        value -= taxValue; // Subtraindo taxas do valor inputado
-        if ((value / valuation).toFixed(5) < 0) value = 0 // Zerando valor caso resultado da soma seja negativo
-        setPrice((value / valuation).toFixed(5)) // Setando preço
-    }
-
-    // Essa função retorna o valor do token em reais inflacionado.
-    const getValuation = async () => {
-        await fetch(`https://api.1inch.io/v4.0/137/quote?fromTokenAddress=${mainToken}&toTokenAddress=${token.TokenAddress}&amount=${1000000}`)
-            .then(res => res.json())
-            .then(async json => {
-                const tokenAmountForValue = (json.toTokenAmount / Math.pow(10, json.toToken.decimals))
-                await fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL')
-                    .then(resp => resp.json())
-                    .then(json => {
-                        let value = (json.USDBRL.bid * 1.003) * (1 / tokenAmountForValue)
-                        value += value * 0.010 // Inflação de 1%
-                        setValuation(value)
-                    })
-                    .catch(error => {
-                        {throw error}
-                        setValuation(0)
-                    })
-            })
-            .catch(error => {
-                {throw error}
-                setValuation(0)
-            })
-    }
-
-    React.useEffect(() => {
-        const number = localStorage.getItem("buyValue")
-        getValuation()
-        getFee(number)
-    }, [token, valuation])
-
-    // Counter
-    React.useEffect(() => {
-        setTimeout(() => {
-            if (counter === 0) {
-                getValuation()
-                setTimeout(() => {
-                    setCounter(30)
-                }, 1000);
-            }
-            else setCounter(counter - 1)
-        }, 1000);
-    }, [counter])
-
-    return (
-        <div style={entranceConfig}>
-            <InputBRL name="buyValue" label="Escolha o valor" onChange={handleConversion} value={payoff} />
-            <SelectorContext.Provider value={{ isOpen, setIsOpen }}>
-                <InputCoin name="buyValue" label="Após a compra você receberá" distance="1rem" value={price} reload={counter} />
-            </SelectorContext.Provider>
-            <Summary coin={token.TokenSymbol} gas={gasFee} tax={tax} price={valuation} distance="1rem" />
-        </div>
+  const handleBinance = async (amount, tokenAddress) => {
+    let MATIC;
+    await fetch(
+      `https://api.1inch.io/v4.0/137/quote?fromTokenAddress=0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174&toTokenAddress=${tokenAddress}&amount=${amount}`
     )
-}
+      .then((response) => response.json())
+      .then((json) => {
+        MATIC = json.toTokenAmount;
+      })
+      .catch((error) => {
+        MATIC = 0;
+        throw error;
+      });
+    return MATIC;
+  };
 
-export default StepA
+  const handleGasFee = async () => {
+    const web3Provider = new ethers.providers.JsonRpcProvider(
+      "https://polygon-mainnet.g.alchemy.com/v2/guTQ9wHBaJDSnRVDjgo1nL6SSqYLpVWb"
+    );
+    const feeData = await web3Provider.getGasPrice();
+    let gasUsed = 200;
+    let finalGasUsedPriced = parseInt(feeData) * gasUsed * 1.3;
+    let MATIC = await handleBinance(
+      "1000000",
+      "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
+    );
+    let MaticResult = finalGasUsedPriced / parseInt(MATIC);
+    await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL")
+      .then((resp) => resp.json())
+      .then((json) => {
+        let dollarValue = parseFloat(json.USDBRL.bid);
+        let valorFinal = MaticResult * dollarValue;
+        MATIC = valorFinal + 0.2;
+      })
+      .catch(() => {
+        MATIC = 0;
+      });
+    return MATIC;
+  };
+
+  const getFee = (number) => {
+    let value = number;
+    let taxValue = 0;
+    taxValue += number * 0.017;
+    if (number > 62.5) taxValue += number * 0.008;
+    else taxValue += 0.5;
+    if (value >= 20) {
+      setTax(taxValue);
+      setGasFee(asyncFee);
+    } else {
+      setTax(0);
+      setGasFee(0);
+    }
+
+    value -= taxValue;
+    if ((value / valuation).toFixed(5) < 0) value = 0;
+    setPrice((value / valuation).toFixed(5));
+  };
+
+  // Essa função retorna o valor do token em reais inflacionado.
+  const getValuation = async () => {
+    await fetch(
+      `https://api.1inch.io/v4.0/137/quote?fromTokenAddress=${mainToken}&toTokenAddress=${
+        token.TokenAddress
+      }&amount=${1000000}`
+    )
+      .then((res) => res.json())
+      .then(async (json) => {
+        const tokenAmountForValue =
+          json.toTokenAmount / Math.pow(10, json.toToken.decimals);
+        await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL")
+          .then((resp) => resp.json())
+          .then((json) => {
+            let value = json.USDBRL.bid * 1.003 * (1 / tokenAmountForValue);
+            value += value * 0.01; // Inflação de 1%
+            setValuation(value);
+          })
+          .catch(() => {
+            setValuation(0);
+          });
+      })
+      .catch(() => {
+        setValuation(0);
+      });
+  };
+
+  React.useEffect(() => {
+    if (localStorage.getItem("buyValue") >= 20) {
+      setGasFee(asyncFee)
+    }
+  }, [asyncFee])
+
+  React.useEffect(() => {
+    (async () => {
+      setAsyncFee(await handleGasFee());
+    })();
+    setInterval(() => {
+      (async () => {
+        setAsyncFee(await handleGasFee());
+      })();
+    }, 30000);
+  }, []);
+
+  React.useEffect(() => {
+    const number = localStorage.getItem("buyValue");
+    getValuation();
+    getFee(number);
+  }, [token, valuation]);
+
+  // Counter
+  React.useEffect(() => {
+    setTimeout(() => {
+      if (counter === 0) {
+        getValuation();
+        setTimeout(() => {
+          setCounter(30);
+        }, 1000);
+      } else setCounter(counter - 1);
+    }, 1000);
+  }, [counter]);
+
+  return (
+    <div style={entranceConfig}>
+      <InputBRL
+        name="buyValue"
+        label="Escolha o valor"
+        onChange={handleConversion}
+        value={payoff}
+      />
+      <SelectorContext.Provider value={{ isOpen, setIsOpen }}>
+        <InputCoin
+          name="buyValue"
+          label="Após a compra você receberá"
+          distance="1rem"
+          value={price}
+          reload={counter}
+        />
+      </SelectorContext.Provider>
+      <Summary
+        coin={token.TokenSymbol}
+        gas={gasFee}
+        tax={tax}
+        price={valuation}
+        distance="1rem"
+      />
+    </div>
+  );
+};
+
+export default StepA;
